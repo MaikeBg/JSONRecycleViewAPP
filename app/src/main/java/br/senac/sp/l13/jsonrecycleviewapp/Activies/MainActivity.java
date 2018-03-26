ce418211253791a24255bb14419689472404d09a
MAIKE CHRISTOPHER DA SILVA BORGES <maike.csborges@senacsp.edu.br>
2018-03-26 10:45:51 -0300

    Initial commit

---
+++ app/src/main/java/br/senac/sp/l13/jsonrecycleviewapp/Activies/MainActivity.java
@@ -0,0 +1,15 @@
+package br.senac.sp.l13.jsonrecycleviewapp.Activies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.senac.sp.l13.jsonrecycleviewapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/br/senac/sp/l13/jsonrecycleviewapp/Model/Anime.java
@@ -0,0 +1,8 @@
+package br.senac.sp.l13.jsonrecycleviewapp.Model;
+
+/**
+ * Created by Maike.csborges on 26/03/2018.
+ */
+
+public class Anime {
+}
